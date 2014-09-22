f42e9c629b7f61bf1b63f3acc1bf0cbda4b998bf
ganangardiy <mr.ganang@gmail.com>
2014-09-22 21:43:20 +0700

    form 2
    cekkk

--- application/views/booking/index.php
+++ application/views/booking/index.php
@@ -47,6 +47,14 @@
 				<div class="panel panel-primary">
 					<div class="panel-heading">Datepicker</div>
 					<div class="panel-body">
+
+						<div class="row">
+							<div class="col-md-12 column">
+								<div id="notif" class="alert alert-warning" role="alert" style="display:none;">Sorry, there isn’t a table matching your selection.
+									You can check availability by changing the date, party size or session.
+								</div>
+							</div>
+						</div>
 						<div class="row">
 							<div class="col-md-6 column col-md-offset-1">
 								<div class="date" >
@@ -55,59 +63,59 @@
 							</div>
 							<div class="col-md-4 column">
 								<div class="form-group">
-								<center><h5><b>
+									<center><h5><b>
 										<div class="disp"></div>
 									</b>
-									</h5>
-									</center>
-								</div>
-								<div class="form-group">
-									<select class="form-control block">
-										<option value="1">1 Person</option>
-										<option value="2">2 Person</option>
-										<option value="3">3 Person</option>
-										<option value="4">4 Person</option>
-										<option value="5">5 Person</option>
-									</select>
-								</div>
-								<div class="form-group">
-									<select class="form-control block">
-										<option value="pagi">Breakfast</option>
-										<option value="siang">Lunch</option>
-										<option value="malam">Dinner</option>
-									</select>
-								</div>
-								<div class="form-group">
-									<button type="button" class="btn btn-lg btn-block btn-primary" disabled="disabled">Booking</button>
-								</div>
-								<div class="alert alert-warning" role="alert">Not Available</div>
-							</div>
-							
+								</h5>
+							</center>
 						</div>
+						<div class="form-group">
+							<select class="form-control block">
+								<option value="1">1 Person</option>
+								<option value="2">2 Person</option>
+								<option value="3">3 Person</option>
+								<option value="4">4 Person</option>
+								<option value="5">5 Person</option>
+							</select>
+						</div>
+						<div class="form-group">
+							<select class="form-control block">
+								<option value="pagi">Breakfast</option>
+								<option value="siang">Lunch</option>
+								<option value="malam">Dinner</option>
+							</select>
+						</div>
+						<div class="form-group">
+							<button type="button" class="btn btn-lg btn-block btn-primary" disabled="disabled">Booking</button>
+						</div>
+						<div class="alert alert-warning" role="alert">Not Available</div>
+					</div>
+					
+				</div>
 
-					</div>
-				</div>
-			</div>
-			<div class="col-md-6 column" >
-				<div id="notif" class="alert alert-warning" role="alert" style="display:none;">Sorry, there isn’t a table matching your selection.
-					You can check availability by changing the date, party size or session.
-				</div>
-				<div class="panel panel-primary">
-					<div class="panel-heading">Select an option below:</div>
-					<div class="panel-body">
-						Standard À La Carte <button type="button" class="btn btn-sm btn-primary col-md-offset-6">Booking</button>
-					</div>
-				</div>
 			</div>
 		</div>
 	</div>
-	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-	<script src="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.js"></script>
-	<script>
-		$('.date').datepicker({ 
-			todayHighlight: true
-		}).on('changeDate', function(e){ 
+	<div class="col-md-6 column" >
+		<div id="notif" class="alert alert-warning" role="alert" style="display:none;">Sorry, there isn’t a table matching your selection.
+			You can check availability by changing the date, party size or session.
+		</div>
+		<div class="panel panel-primary">
+			<div class="panel-heading">Select an option below:</div>
+			<div class="panel-body">
+				Standard À La Carte <button type="button" class="btn btn-sm btn-primary col-md-offset-6">Booking</button>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.js"></script>
+<script>
+	$('.date').datepicker({ 
+		todayHighlight: true
+	}).on('changeDate', function(e){ 
    				// $('.disp').html(e.format('dd-M-yyyy') + "<br>"+e.format('dd-MM-yyyy'));// or use e.date manually
    				$('.disp').html(e.format('dd-MM-yyyy'));  
    				checkingDateEngage(e);
